2c23a43fdbc001433f33566fca66accb7cd4dc27
Igor Ribeiro <igorribeiromattos@gmail.com>
2019-07-17 14:19:32 -0300

    Facebook login via facebook api sem db

--- app/src/main/java/com/igor/geenker/MainActivity.java
+++ app/src/main/java/com/igor/geenker/MainActivity.java
@@ -1,52 +1,140 @@
 package com.igor.geenker;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.telecom.Call;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static com.facebook.login.LoginBehavior.WEB_ONLY;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CircleImageView circleImageView;
+    private TextView textName, txtEmail;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        loginButton = findViewById(R.id.login_button);
+        textName = findViewById(R.id.profile_nome);
+        txtEmail = findViewById(R.id.profile_mail);
+        circleImageView = findViewById(R.id.profile_image);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        checkLoginStatus();
+
+        //loginButton.setLoginBehavior(WEB_ONLY);
+
+        loginButton.setPermissions(Arrays.asList("email", "public_profile"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
             }
         });
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken == null){
+                textName.setText("");
+                txtEmail.setText("");
+                circleImageView.setImageResource(0);
+                Toast.makeText(MainActivity.this, "User Logged out", Toast.LENGTH_LONG);
+            }
         }
+    };
 
-        return super.onOptionsItemSelected(item);
+    private void loaduserprofile (AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                    String image_url = "https://graph.facebook.com/"+id+"/picture?type=normal";
+
+                    txtEmail.setText(email);
+                    textName.setText(first_name + " " + last_name);
+                    RequestOptions requestOptions = new RequestOptions();
+                    requestOptions.dontAnimate();
+
+                    Glide.with(MainActivity.this).load(image_url).into(circleImageView);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameter = new Bundle();
+        parameter.putString("fields", "first_name, last_name, email, id");
+        request.setParameters(parameter);
+        request.executeAsync();
+    }
+
+    private void checkLoginStatus(){
+        if(AccessToken.getCurrentAccessToken()!=null){
+            loaduserprofile(AccessToken.getCurrentAccessToken());
+        }
     }
 }
